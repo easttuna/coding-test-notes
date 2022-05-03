cbb3094a243742c7ab83e706c9414e303d511a30
easttuna <jomin8393@gmail.com>
2022-05-03 20:15:16 +0900

    DP cases added

--- Type6_DP/DP_ex01_1로만들기.py
+++ Type6_DP/DP_ex01_1로만들기.py
@@ -1,52 +1,16 @@
-# 4.26 (화)
-# 23:05~
-# 20분
-
-# 탐색과정, 그러나 계산결과를 저장해두는 방식
-# 연산의 종류가 a,b,c,d가 있으므로 각 종류별 딕셔너리를 만들어서 메모이제이션
-
-from collections import defaultdict, deque
 
 x = int(input())
-dp = defaultdict(int)
-queue = deque()
-queue.append(x)
 
-while queue:
-    n = queue.popleft()
-    c_cnt = dp[n]
+dp = [0] * (x+1)
 
-    if n%5 == 0:
-        temp = n//5
-        if temp > 0:
-            queue.append(temp)
-            if dp[temp] == 0:
-                dp[temp] = c_cnt +1
-            else:
-                dp[temp] = min(c_cnt+1, dp[temp])
-    if n%3 == 0:
-        temp = n//3
-        if temp > 0:
-            queue.append(temp)
-            if dp[temp] == 0:
-                dp[temp] = c_cnt +1
-            else:
-                dp[temp] = min(c_cnt+1, dp[temp])
-    if n%2 == 0:
-        temp = n//2
-        if temp > 0:
-            queue.append(temp)
-            if dp[temp] == 0:
-                dp[temp] = c_cnt +1
-            else:
-                dp[temp] = min(c_cnt+1, dp[temp])
-    if n > 1:
-        temp = n-1
-        queue.append(temp)
-        if dp[temp] == 0:
-            dp[temp] = c_cnt +1
-        else:
-            dp[temp] = min(c_cnt+1, dp[temp])
+for i in range(2, x+1):
+    dp[i] = dp[i - 1] + 1
+    if i % 5 == 0:
+        dp[i] = min(dp[i], dp[i // 5] + 1)
+    if i % 3 == 0:
+        dp[i] = min(dp[i], dp[i // 3] + 1)
+    if i % 2 == 0:
+        dp[i] = min(dp[i], dp[i // 2] + 1)
 
-print(dp[1])
-
+print(dp[x])
+    
